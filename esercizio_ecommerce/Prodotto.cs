3f428c30e585030d0bcbf8eff4acec30bb530852
ghila05 <nicolaghi05@gmail.com>
2022-11-16 15:57:24 +0100

    Add project files.

---
+++ esercizio_ecommerce/Prodotto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace esercizio_ecommerce
+{
+    class Prodotto
+    {
+    }
+}
---
+++ esercizio_ecommerce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace esercizio_ecommerce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
