c185afdf41b1247f227ab01ffddd83c93ec2611a
ghila05 <nicolaghi05@gmail.com>
2022-11-16 16:47:36 +0100

    implementazione

--- esercizio_ecommerce/Carrello.cs
+++ esercizio_ecommerce/Carrello.cs
@@ -7,32 +7,39 @@ namespace esercizio_ecommerce
     class Carrello
     {
         string _id;
-
-
+        public Prodotto[] p = new Prodotto[10];
+        public int counter;
         public Carrello(string id)
         {
             _id = id;
         }
 
-        public void Aggiungi(Prodotto p)
+        public void Aggiungi(Prodotto n)
         {
-
+            counter = counter + 1;
+            p[counter+1] = n;
         }
-        public void Rimuovi(Prodotto p)
+        public void Rimuovi(Prodotto n)
         {
-
+            for (int i=0; i<p.Length;i++)
+            {
+                if (p[i] == n)
+                {
+                    p[i] = null;
+                }
+            }
         }
         public Prodotto [] Svuota()
         {
-          Prodotto[] p = new Prodotto[10];
-
+            for (int i = 0; i < p.Length; i++)
+            {
+                p[i] = null;
+            }
             return p;
         }
 
-        public Prodotto[] getProdotto()
+        public Prodotto[] getProdotti()
         {
-            Prodotto[] p = new Prodotto[10];
-
             return p;
 
         }
