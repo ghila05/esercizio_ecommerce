50986f53b3907c5b400c08edc85830e3d64716cd
ghila05 <nicolaghi05@gmail.com>
2022-11-16 16:39:16 +0100

    implementazione classi

--- esercizio_ecommerce/Carrello.cs
+++ esercizio_ecommerce/Carrello.cs
@@ -6,5 +6,39 @@ namespace esercizio_ecommerce
 {
     class Carrello
     {
+        string _id;
+
+
+        public Carrello(string id)
+        {
+            _id = id;
+        }
+
+        public void Aggiungi(Prodotto p)
+        {
+
+        }
+        public void Rimuovi(Prodotto p)
+        {
+
+        }
+        public Prodotto [] Svuota()
+        {
+          Prodotto[] p = new Prodotto[10];
+
+            return p;
+        }
+
+        public Prodotto[] getProdotto()
+        {
+            Prodotto[] p = new Prodotto[10];
+
+            return p;
+
+        }
+
+
+
+
     }
 }
--- esercizio_ecommerce/Prodotto.cs
+++ esercizio_ecommerce/Prodotto.cs
@@ -6,5 +6,29 @@ namespace esercizio_ecommerce
 {
     class Prodotto
     {
+        string _id;
+        string _nome;
+        float _prezzo;
+        string _produttore;
+        string _descrizione;
+
+        public Prodotto(string id, string nome, float prezzo, string produttore, string descrizione)
+        {
+            _id = id;
+            _nome = nome;
+            _prezzo = prezzo;
+            _produttore = produttore;
+            _descrizione = descrizione;
+        }
+
+        private void setPrezzo(float prezzo)
+        {
+            _prezzo = prezzo;
+        }
+
+ 
+
+
+
     }
 }
